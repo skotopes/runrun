29774c296fdec2be392d83417c5904b8a5b51be0
Alexandr Kutuzov <alleteam@gmail.com>
2015-02-16 21:34:39 +0900

    Injects for analytics

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ SQLALCHEMY_DATABASE_URI	= 'postgres://localhost/runrun'
 GOOGLE_MAPS_API_KEY		= ''
 MAIL_DEFAULT_SENDER		= 'runrun@localhost'
 MAIL_ADMIN_EMAILS		= ['root@localhost']
+TRACKING_CODE			= ''
 
 if path.isfile('config_local.py'):
 	execfile('config_local.py')
